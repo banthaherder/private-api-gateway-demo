0e0a403c960575d8f4903137875325a5e7d7d2e2
Dawson Mortenson <dawson.mortenson@gmail.com>
2019-02-04 21:09:26 -0800

    update demo to use single public and single public subnets in which the private api gateway endpoint is accessible from

--- infra/vpc.tf
+++ infra/vpc.tf
@@ -21,25 +21,14 @@ resource "aws_subnet" "private_subnet_az1" {
   depends_on = ["aws_vpc.private_api_gateway_demo_vpc"]
 }
 
-resource "aws_subnet" "private_subnet_az2" {
-  vpc_id            = "${aws_vpc.private_api_gateway_demo_vpc.id}"
-  cidr_block        = "172.25.16.0/20"
-  availability_zone = "${data.aws_availability_zones.available.names[1]}"
+resource "aws_subnet" "public_subnet_az2" {
+  vpc_id                  = "${aws_vpc.private_api_gateway_demo_vpc.id}"
+  cidr_block              = "172.25.16.0/20"
+  availability_zone       = "${data.aws_availability_zones.available.names[0]}"
+  map_public_ip_on_launch = true
 
   tags = {
-    Name = "PrivateSubnetAZ2"
-  }
-
-  depends_on = ["aws_vpc.private_api_gateway_demo_vpc"]
-}
-
-resource "aws_subnet" "private_subnet_az3" {
-  vpc_id            = "${aws_vpc.private_api_gateway_demo_vpc.id}"
-  cidr_block        = "172.25.32.0/20"
-  availability_zone = "${data.aws_availability_zones.available.names[2]}"
-
-  tags = {
-    Name = "PrivateSubnetAZ3"
+    Name = "PublicSubnetAZ2"
   }
 
   depends_on = ["aws_vpc.private_api_gateway_demo_vpc"]
@@ -53,10 +42,26 @@ resource "aws_internet_gateway" "demo_vpc_igw" {
   }
 }
 
-resource "aws_route" "demo_vpc_default_route" {
-  route_table_id         = "${aws_vpc.private_api_gateway_demo_vpc.default_route_table_id}"
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = "${aws_internet_gateway.demo_vpc_igw.id}"
+resource "aws_route_table" "public_route_table" {
+  vpc_id = "${aws_vpc.private_api_gateway_demo_vpc.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.demo_vpc_igw.id}"
+  }
+
+  tags = {
+    Name = "DemoRouteTableForPublicSubnet"
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_association" {
+  subnet_id      = "${aws_subnet.public_subnet_az2.id}"
+  route_table_id = "${aws_route_table.public_route_table.id}"
+
+  depends_on = [
+    "aws_subnet.public_subnet_az2",
+  ]
 }
 
 resource "aws_vpc_endpoint" "api_gateway" {
@@ -72,14 +77,10 @@ resource "aws_vpc_endpoint" "api_gateway" {
 
   subnet_ids = [
     "${aws_subnet.private_subnet_az1.id}",
-    "${aws_subnet.private_subnet_az2.id}",
-    "${aws_subnet.private_subnet_az3.id}",
   ]
 
   depends_on = [
     "aws_security_group.allow_https",
     "aws_subnet.private_subnet_az1",
-    "aws_subnet.private_subnet_az2",
-    "aws_subnet.private_subnet_az3",
   ]
 }
